a0bd31bb7e55ea8964c718bae27d22669d5b605d
ffff fffffff <ffff@berkeley.edu>
2018-09-29 16:38:27 -0700

    1.0.1

--- command.js
+++ command.js
@@ -19,7 +19,7 @@ Print as ISO_8601 formatted duration. https://en.wikipedia.org/wiki/ISO_8601#Dur
 `
 let argv = require('minimist')(process.argv.slice(2));
 let path = argv._[0]
-let sumOrg = require('.').sumOrg
+let sumOrg = require('.')
 sumOrg(path, duration => {
 
     if (argv.help) {
