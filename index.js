9a2d3d759d102a4d8ae2044f040eb54898fcec9c
ffff fffffff <ffff@berkeley.edu>
2018-09-29 15:53:22 -0700

    Working example

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-module.exports = 'hello'
+let { compose } = require('tinyfunk')
+let { duration } = require('moment')
+let org = require('org-mode-parser')
+
+// List[String] => moment.duration
+function sumLogbook (logbook) {
+    let truthy = x => !!x
+    let listify = _ =>
+        _.split('\n').filter(truthy)
+    let splitDuration = _ =>
+        _.split('=>  ')[1]
+    let splitDurations = _ =>
+        _.map(splitDuration).map(duration)
+    let sumDurations = _ => _.reduce((a, b) => a.add(b))
+    return compose(
+        sumDurations,
+        splitDurations,
+        listify
+    )(logbook)
+}
+
+// String -> moment.duration
+function sumOrg (pathToOrgFile, cb) {
+    org.makelist(pathToOrgFile, nodes => {
+        let ofd = new org.OrgQuery(nodes)
+        let logbook = _ => _.drawer['LOGBOOK']
+        let combinedLogbook = ofd.toArray().map(logbook).join('')
+        cb(sumLogbook(combinedLogbook))
+    })
+}
+
+module.exports = {
+    sumLogbook:sumLogbook,
+    sumOrg:sumOrg,
+}
--- test/test.js
+++ test/test.js
@@ -1,7 +1,24 @@
 let test = require('tape')
-let text = require('..')
+let investments = require('..')
+let readFileSync = require('fs').readFileSync
 
-test('hello', t => {
-    t.deepEquals(text, 'hello')
+let logbook =
+    readFileSync('./test/logbook-drawer.txt', 'utf-8')
+
+test('sumLogbook sums the times in an org log book', t=> {
+    let times = investments.sumLogbook(logbook)
+    t.deepEquals(
+        times.minutes(),
+        30,
+    )
     t.end()
+})
+
+let orgPath = './test/org-file.org'
+
+test('sumOrg all the times in an org file', t => {
+    investments.sumOrg(orgPath, function (duration) {
+        t.deepEquals(duration.minutes(), 10)
+        t.end()
+    })
 })
